10c8c23324cd2f6a3851976fceee7a9a6122c8af
Roman Kononov <roman.kononov@hashcoins.com>
2020-10-08 20:09:49 +0300

    update

--- hw1_ntp.go
+++ hw1_ntp.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	var ntpTime time.Time
-	ntpTime = ntpGetTime("time.apple.com")
-	//ntpTimeFormatted := ntpTime.Format(time.UnixDate)
+	var ntpHost string
+	fmt.Println("Enter ntp host: ")
+	fmt.Scanln(&ntpHost)
+
+	ntpTime = ntpGetTime(ntpHost)
+	ntpTimeFormatted := ntpTime.Format(time.UnixDate)
 	fmt.Printf("Network time: %v\n", ntpTime)
-	//fmt.Printf("Unix Date Network time: %v\n", ntpTimeFormatted)
+	fmt.Printf("Unix Date Network time: %v\n", ntpTimeFormatted)
 	fmt.Println("+++++++++++++++++++++++++++++++")
 	timeFormatted := time.Now().Local().Format(time.UnixDate)
 	fmt.Printf("System time: %v\n", time.Now())
